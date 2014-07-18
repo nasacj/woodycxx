4f7bacdc90feaeaa4ad6a43af38161b10a2bc81a
nasadiscovery <cj.nasa@gmail.com>
2014-07-18 17:09:45 +0800

    AbstractSocket initial
    Signed-off-by: nasadiscovery <cj.nasa@gmail.com>

---
+++ AbstractSocket.h
@@ -0,0 +1,114 @@
+/*
+ * AbstractSocket.h
+ *
+ *  Created on: 2014-7-18
+ *      Author: qianchj
+ */
+
+#ifndef ABSTRACTSOCKET_H_
+#define ABSTRACTSOCKET_H_
+
+#include "InputStream.h"
+#include "OutputStream.h"
+#include "InetAddress.h"
+#include <string>
+
+using namespace woodycxx::io;
+
+namespace woodycxx { namespace net {
+
+
+
+class AbstractSocket
+{
+protected:
+
+    /**
+     * Connects this socket to the specified port on the named host.
+     *
+     * @param      host   the name of the remote host.
+     * @param      port   the port number.
+     *
+     */
+    virtual void connect(string host, int port) = 0;
+
+    /**
+     * Connects this socket to the specified port number on the specified host.
+     *
+     * @param      address   the IP address of the remote host.
+     * @param      port      the port number.
+     *
+     */
+    virtual void connect(InetAddress& address, int port) = 0;
+
+    /**
+     * Binds this socket to the specified local IP address and port number.
+     *
+     * @param      host   an IP address that belongs to a local interface.
+     * @param      port   the port number.
+     */
+    virtual void bind(InetAddress& host, int port) = 0;
+
+    /**
+     * Sets the maximum queue length for incoming connection indications
+     * (a request to connect) to the <code>count</code> argument. If a
+     * connection indication arrives when the queue is full, the
+     * connection is refused.
+     *
+     * @param      backlog   the maximum length of the queue.
+     *
+     */
+    virtual void listen(int backlog) = 0;
+
+    /**
+     * Accepts a connection.
+     *
+     * @param      s   the accepted connection.
+     *
+     */
+    virtual void accept(AbstractSocket& s) = 0;
+
+    /**
+     * Returns an input stream for this socket.
+     *
+     * @return     a stream for reading from this socket.
+     *
+    */
+    virtual InputStream& getInputStream() = 0;
+
+
+    /**
+     * Returns an output stream for this socket.
+     *
+     * @return     an output stream for writing to this socket.
+     * @exception  IOException  if an I/O error occurs when creating the
+     *               output stream.
+     */
+    virtual OutputStream& getOutputStream() = 0;
+
+    /**
+     * Returns the number of bytes that can be read from this socket
+     * without blocking.
+     *
+     * @return     the number of bytes that can be read from this socket
+     *             without blocking.
+     * @exception  IOException  if an I/O error occurs when determining the
+     *               number of bytes available.
+     */
+    virtual int available()  = 0;
+
+    /**
+     * Closes this socket.
+     *
+     * @exception  IOException  if an I/O error occurs when closing this socket.
+     */
+    virtual void close() = 0;
+
+
+
+    virtual ~AbstractSocket() {}
+
+
+}}
+
+#endif /* ABSTRACTSOCKET_H_ */
