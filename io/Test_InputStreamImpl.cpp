2c64c2e8775f78367a3e437c55380bda12b3e218
nasadiscovery <cj.nasa@gmail.com>
2014-09-17 16:12:15 +0800

    Update Test_InputStreamImpl for read(void*) func

--- io/Test_InputStreamImpl.cpp
+++ io/Test_InputStreamImpl.cpp
@@ -21,6 +21,17 @@ int main()
     InputStreamImpl impl;
     impl.InputStream::read(buf);
     //impl.read(buf);
+
+    uint8* bytes = new uint8[10];
+    inputstream.read(bytes, 10);
+
+    std::cout << "bytes new[10] reading..." << std::endl;
+    for (int i = 0; i < buf.getSize(); i++)
+    {
+        std::cout << static_cast<int>(buf[i]) << " ";
+    }
+    std::cout << std::endl;
+
     impl.hello();
 
     return 0;
